f97d892080707a2bac787eb45ee591c61af505f3
fkotyashichev <fkotyashichev@gmail.com>
2023-09-14 14:02:00 +0300

    fixed keyboard.py

--- src/keyboard.py
+++ src/keyboard.py
@@ -2,8 +2,21 @@ from src.item import Item
 
 
 class MixLanguage:
-    pass
+    def __init__(self, name, price, quantity, language):
+        super().__init__(name, price, quantity)
+        self.__language = language
+
+    @property
+    def language(self):
+        return self.__language
+
+    def change_lang(self):
+        if self.__language == "EN":
+            self.__language = "RU"
+        else:
+            self.__language = "EN"
 
 
 class Keyboard(MixLanguage, Item):
-    pass
+    def __init__(self, name: str, price: float, quantity: int, language="EN"):
+        super().__init__(name, price, quantity,language)

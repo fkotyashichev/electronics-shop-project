1db76f648073eb8090e5ea500deead89f83b2b5c
fkotyashichev <fkotyashichev@gmail.com>
2023-09-14 14:01:09 +0300

    fixed test

--- tests/test_keyboard.py
+++ tests/test_keyboard.py
@@ -6,13 +6,15 @@ import pytest
 def kb():
     return Keyboard('Dark Project KD87A', 9600, 5)
 
+
 def test_magic(kb):
     assert str(kb) == "Dark Project KD87A"
+
 
 def test_language(kb):
     assert str(kb.language) == "EN"
     kb.change_lang()
     assert str(kb.language) == "RU"
 
-    with pytest.raises(AttributeError)
+    with pytest.raises(AttributeError):
         kb.language = 'CH'
